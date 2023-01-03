e737e0c12cc63717b48e1fd6723e2205f1252b1d
Wasuphat.pud <narayutput@gmail.com>
2023-01-03 13:31:33 +0700

    add: home header

--- frontend/src/views/home-page.vue
+++ frontend/src/views/home-page.vue
@@ -1,7 +1,10 @@
 <template>
   <b-row id="homePage">
     <b-col cols="12" class="mt-3">
-      
+      <h2 class="d-flex align-items-baseline">
+        <font-awesome-icon icon="home" />
+        <span class="ml-2">Home</span>
+      </h2>
     </b-col>
   </b-row>
 </template>
