8fc31c55f23a27b412975271a92f78717cbd39ec
Wasuphat.pud <narayutput@gmail.com>
2023-01-03 16:34:42 +0700

    update: solution create todo list

--- backend/api/todo-list/controllers/todo-list.js
+++ backend/api/todo-list/controllers/todo-list.js
@@ -1,8 +1,29 @@
 'use strict';
 
+const { sanitizeEntity } = require('strapi-utils');
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async createMyTodoList(ctx) {
+    const { user } = ctx.state;
+    const { title, description = null } = ctx.request.body;
+
+    if (!title) {
+      throw strapi.errors.badData(
+        "title is required."
+      );
+    }
+
+    const entity = await strapi.services["todo-list"].create({
+      title,
+      description,
+      user: user.id
+    });
+
+    return sanitizeEntity(entity, { model: strapi.models["todo-list"] });
+  }
+};
