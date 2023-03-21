6b2b10017a8603a9709589ef66fc86c292823442
Panisara Lakhan <panisara.nampun@gmail.com>
2023-03-21 17:16:54 +0700

    update: add globalApiConfig Authorization

--- frontend/src/models/User.js
+++ frontend/src/models/User.js
@@ -12,6 +12,41 @@ export class User extends Model {
       confirmed: this.boolean(false)
     }
   }
+  
+  static globalApiConfig = {
+    headers: localStorage.getItem("jwt")!==null?{ 'Authorization':"Bearer "+ localStorage.getItem("jwt")  }:{}
+  }
+  static apiConfig = {
+    actions: {
+      login(body) {
+        // console.log("dsdsihfdslsasa");
+        return this.post(`/auth/local`,body,{ save: false ,
+            dataTransformer:(res)=>{
+                localStorage.setItem("jwt", res.data.jwt)
+                return res.data.user
+            }
+        })
+      },
+        load(){
+        return this.get(`/users/me`)
+    }
+    },
+
+  }
+
+  static fetchById (id) {
+    return this.api().get(`/api/users/${id}`)
+  }
+
+  static async login(body){
+    // console.log("fdshfdsds");
+    let res = await this.api().login(body)
+    // console.log(res);
+    await res.save()
+  }
+
+
+  
 }
 
 export default User;
--- frontend/src/views/home-page.vue
+++ frontend/src/views/home-page.vue
@@ -10,13 +10,39 @@
       <b-button title="About" :to="{ name: 'about' }"><font-awesome-icon :icon="['fas', 'info']" />
         About</b-button>
     </b-row>
+    <!-- form input -->
+    <b-form @submit="onSubmit" class="my-2">
+      <b-form-group label="Email">
+        <b-form-input v-model="email" type="email"></b-form-input>
+      </b-form-group>
+      <b-form-group label="Password">
+        <b-form-input v-model="password" type="password"></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Submit</b-button>
+    </b-form>
   </div>
 </template>
 
 <script>
+import User from '@/models/User';
 export default {
   data() {
-    return {};
+    return {
+      email: "nampun@mail.com",
+      password: "123456"
+    };
   },
+  methods: {
+    async onSubmit(event) {
+      event.preventDefault()
+      // let { email, password } = this 
+
+      let body = { identifier: this.email, password: this.password }
+      // let user = new User({ email, password })
+
+      await User.login(body)
+    },
+
+  }
 };
 </script>
