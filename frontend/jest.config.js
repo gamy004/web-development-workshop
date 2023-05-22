910b9d106d01bf50260d9b42d8fb958d54b4b118
Win Nattawat <nattawat.r@ku.th>
2023-05-22 23:53:45 +0700

    jest ignore file for testing

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,5 +2,15 @@ module.exports = {
 	preset: "@vue/cli-plugin-unit-jest",
 	transformIgnorePatterns: ["/node_modules/(?!bootstrap-vue)/"],
 	collectCoverage: true,
-	collectCoverageFrom: ["**/*.{js,vue}", "!**/node_modules/**"],
+	collectCoverageFrom: [
+		"**/*.{js,vue}",
+		"!**/node_modules/**",
+		"!**/*.config.js",
+		"!**/coverage/**",
+		"!**/mixins/**",
+		"!**/icon.js",
+		"!**/main.js",
+		"!**/router.js",
+		"!**/store.js",
+	],
 };
